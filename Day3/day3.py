a9417602a3a9ee1a2f26126acef5ec010533bc78
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-12-12 17:00:09 +0800

    Part two started
    data captured, calculations to be attempted and validated

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,8 +1,108 @@
+# PART ONE
+# grid = []
+# gridline_length = 0
+# symbols = []
+# potential_parts_list = []
+# sum_of_valid_part_numbers = 0
+
+# class Potential_part:
+#     def __init__(self, value, row, index, length):
+#         self.value = value
+#         self.row = row
+#         self.index = index
+#         self.length = length
+
+# def validate_part(grid, part):
+#     # print(f"{part.value} in validate loop")
+#     validity = False
+#     if part.row == 0:
+#         row_start = 0
+#         row_end = 1
+#     elif part.row == (len(grid) - 1):
+#         row_start = -1
+#         row_end = 0
+#     else:
+#         row_start = -1
+#         row_end = 1
+
+#     if part.index == 0:
+#         index_start = 0
+#         index_end = index_start + part.length + 1
+#     elif part.index + part.length + 1 > gridline_length - 1:
+#         index_start = -1
+#         index_end = index_start + part.length + 1
+#     else:
+#         index_start = -1
+#         index_end = index_start + part.length + 2
+
+#     for r in range(row_start, (row_end + 1)):
+#         # print(f"row iteration {r}")
+#         for item in range(index_start, (index_end)):
+#             # print(f"index iteration {item}")
+#             character = grid[part.row + r][part.index + item]
+#             # print(character)
+#             if character in symbols:
+#                 # print(character, part.value, part.row, part.index)
+#                 validity = True
+#             else:
+#                 # print("No adjacent symbols")
+#                 pass
+#     return validity
+
+
+
+# with open ("input.txt", encoding="UTF-8", mode="r") as file:
+#     reader = file.readlines()
+
+#     for line in range(len(reader)):
+#         # print(reader[line])
+#         row = []
+#         gridline_length = len(reader[line])
+#         for char in range(gridline_length):
+#             # print(reader[line][char])
+#             number = reader[line][char]
+#             row.append(number)
+#             if not (reader[line][char].isdigit() or reader[line][char] == '.' or reader[line][char] == '\n') and reader[line][char] not in symbols:
+#                 symbols.append(reader[line][char])
+#             if reader[line][char].isdigit() and reader[line][char-1].isdigit() == False:
+#                 current_length = 0
+#                 number_as_string = ""
+#                 while reader[line][char+current_length].isdigit():
+#                     number_as_string += reader[line][char+current_length]
+#                     current_length += 1
+#                 # print(number_as_string)
+#                 # print(number, current_length)
+#                 part = Potential_part(int(number_as_string), line, char, current_length)
+#                 # print(part.value)
+#                 potential_parts_list.append(part)
+#         grid.append(row)
+
+# # print(gridline_length) = 140
+# count = 0
+
+# for part in potential_parts_list:
+#     # print(f"Part is {part.value}; row is {part.row}, index is {part.index}")
+#     validity = validate_part(grid, part) 
+#     if validity == True:
+#         count += 1
+#         # valid_parts_list.append(part)
+#         # print(f"{part.value} on row {part.row} is a valid part")
+#         sum_of_valid_part_numbers += part.value
+#         # print(f"running total {sum_of_valid_part_numbers}")
+
+# print(sum_of_valid_part_numbers)
+# print(f"Of {len(potential_parts_list)} parts, {count} were valid")
+
+
+
+
+# PART TWO
 grid = []
 gridline_length = 0
-symbols = []
+# symbols = []
 potential_parts_list = []
-sum_of_valid_part_numbers = 0
+gears = []
+# sum_of_valid_part_numbers = 0
 
 class Potential_part:
     def __init__(self, value, row, index, length):
@@ -11,9 +111,18 @@ class Potential_part:
         self.index = index
         self.length = length
 
-def validate_part(grid, part):
+class Gear:
+    def __init__(self, row, index, value):
+        self.row = row
+        self.index = index
+        self.value = value
+    
+    def __repr__(self):
+        return ({self.row}, {self.index}, {self.value})
+
+def find_star_near_numbers(grid, part):
     # print(f"{part.value} in validate loop")
-    validity = False
+    # validity = False
     if part.row == 0:
         row_start = 0
         row_end = 1
@@ -40,13 +149,14 @@ def validate_part(grid, part):
             # print(f"index iteration {item}")
             character = grid[part.row + r][part.index + item]
             # print(character)
-            if character in symbols:
+            if character == '*':
                 # print(character, part.value, part.row, part.index)
-                validity = True
+                potential_gear = Gear((part.row + r), (part.index + item), part.value)
+                gears.append(potential_gear)
             else:
                 # print("No adjacent symbols")
                 pass
-    return validity
+    return
 
 
 
@@ -61,8 +171,8 @@ with open ("input.txt", encoding="UTF-8", mode="r") as file:
             # print(reader[line][char])
             number = reader[line][char]
             row.append(number)
-            if not (reader[line][char].isdigit() or reader[line][char] == '.' or reader[line][char] == '\n') and reader[line][char] not in symbols:
-                symbols.append(reader[line][char])
+            # if not (reader[line][char].isdigit() or reader[line][char] == '.' or reader[line][char] == '\n') and reader[line][char] not in symbols:
+            #     symbols.append(reader[line][char])
             if reader[line][char].isdigit() and reader[line][char-1].isdigit() == False:
                 current_length = 0
                 number_as_string = ""
@@ -76,18 +186,13 @@ with open ("input.txt", encoding="UTF-8", mode="r") as file:
                 potential_parts_list.append(part)
         grid.append(row)
 
-# print(gridline_length) = 140
-count = 0
 
 for part in potential_parts_list:
-    # print(f"Part is {part.value}; row is {part.row}, index is {part.index}")
-    validity = validate_part(grid, part) 
-    if validity == True:
-        count += 1
-        # valid_parts_list.append(part)
-        # print(f"{part.value} on row {part.row} is a valid part")
-        sum_of_valid_part_numbers += part.value
-        # print(f"running total {sum_of_valid_part_numbers}")
-
-print(sum_of_valid_part_numbers)
-print(f"Of {len(potential_parts_list)} parts, {count} were valid")
+    find_star_near_numbers(grid, part) 
+    
+# print(repr(gears))
+for i, gear in enumerate(gears):
+    for comparison_gear in (gears):
+        if gear.index == comparison_gear.index and gear.row == comparison_gear.row and gear.value != comparison_gear.value:
+            print(f"{i} Star with double match at line {gear.row} with index {gear.index} with value {gear.value}")
+      
